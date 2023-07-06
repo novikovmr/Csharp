75badf54e9bebc74eb32d33056ee04419efaa0e6
Max <maksim101101@gmail.com>
2023-07-06 13:28:06 +0400

    Сделал задание 1

--- S1/Ex1/Program.cs
+++ S1/Ex1/Program.cs
@@ -5,5 +5,10 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
+Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) Console.WriteLine($"max = {a}");
+if (b > a) Console.WriteLine($"max = {b}");
